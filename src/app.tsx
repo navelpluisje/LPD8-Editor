c25f3aaf5b54275293b14a1b5ca45825c06c8aa1
Erwin Goossen <erwin@navelpluisje.nl>
2020-02-20 22:19:46 +0100

    test css

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,7 @@ import { Header, Footer } from './components/atoms';
 import { NoMidi } from './components/molecules';
 import { Main } from './components/main';
 import { ConfigContextProvider, MidiContextProvider } from './context';
-import './app.css';
+// import './app.css';
 
 function App() {
   if (navigator.requestMIDIAccess === undefined) {
