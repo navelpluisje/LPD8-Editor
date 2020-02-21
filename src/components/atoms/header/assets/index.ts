af63de96177480aed340c0b596e246104ec3e6a3
Erwin Goossen <erwin@navelpluisje.nl>
2020-02-22 00:37:55 +0100

    added social and more

---
+++ src/components/atoms/header/assets/index.ts
@@ -0,0 +1,9 @@
+import bmac from './bmac.svg';
+import github from './github.svg';
+import twitter from './twitter.svg';
+
+export {
+  bmac,
+  github,
+  twitter,
+};
--- src/components/atoms/header/index.tsx
+++ src/components/atoms/header/index.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import logo from './lpd8-logo.svg';
+import { bmac, github, twitter } from './assets';
+
 import './header.css';
 
 export const Header: React.FC<any> = () => (
   <header className="site-header">
     <img src={logo} alt="The almighty lpd8 logo" />
+    <section className="site-header-contact">
+      <a
+        href="https://twitter.com/navelpluisje_nl"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <img src={twitter} alt="Follow me on Twitter" />
+      </a>
+      <a
+        href="https://github.com/navelpluisje/LPD8-Editor"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <img src={github} alt="Check the code on Github" />
+      </a>
+      <a
+        href="https://www.buymeacoffee.com/navelpluisje"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <img src={bmac} alt="Buy me a beer if you like this page" />
+      </a>
+    </section>
   </header>
 );
